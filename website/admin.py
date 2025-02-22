d9ba06a1ee99d6a0c5ae4aff1915fb222c4dd100
Always-Learningeek <myrealemail1400@gmail.com>
2025-02-23 00:24:58 +0100

    The newsletter is now working

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from website.models import *
+from website.models import Contact, Newsletter
+
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
@@ -7,3 +8,10 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_date', 'subject')
     list_filter = ('email',)
     search_fields = ('name', 'message')
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+    list_filter = ('email',)
+    search_fields = ('email',)
--- website/forms.py
+++ website/forms.py
@@ -2,17 +2,20 @@ from django import forms
 from website.models import Contact, Newsletter
 from captcha.fields import CaptchaField
 
+
 class ContactForm(forms.ModelForm):
     captcha = CaptchaField()
+
     class Meta:
         model = Contact
         fields = ['name', 'email', 'phone_number', 'subject', 'message']
+
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
         self.fields['subject'].required = False
 
-class NewsletterForm(forms.ModelForm):
 
+class NewsletterForm(forms.ModelForm):
     class Meta:
         model = Newsletter
         fields = ['email']
