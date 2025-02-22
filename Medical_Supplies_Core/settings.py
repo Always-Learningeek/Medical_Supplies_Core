af46caf1c5e2c817edb8a69c767db87e03d878b7
Always-Learningeek <myrealemail1400@gmail.com>
2025-02-22 18:07:20 +0100

    requiremnents.txt added
    also settings changed

--- Medical_Supplies_Core/settings.py
+++ Medical_Supplies_Core/settings.py
@@ -19,14 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^5ngjc8=2=-+#xueyw7co_92mw!j1b2-exn#wv9t6^!ct%_==6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -37,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -70,16 +63,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Medical_Supplies_Core.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -117,6 +100,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Medical_Supplies_Core.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Medical_Supplies_Core.setting.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
