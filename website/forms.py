693ebfa552aadc68ffb189e0b48cc6a52b072d13
Always-Learningeek <myrealemail1400@gmail.com>
2025-02-22 21:31:58 +0100

    phone_number  added
    in website/forms.py phone_number  added

--- website/forms.py
+++ website/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from website.models import Contact, Newsletter
 from captcha.fields import CaptchaField
-
+from phonenumber_field.formfields import PhoneNumberField
 
 class ContactForm(forms.ModelForm):
     captcha = CaptchaField()
+    phone_number = PhoneNumberField(region="IR")
     class Meta:
         model = Contact
-        fields = ['name', 'email', 'subject', 'message']
+        fields = ['name', 'email', 'phone_number', 'subject', 'message']
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
         self.fields['subject'].required = False
