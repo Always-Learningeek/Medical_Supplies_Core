28c366b8c0883da2d93a79ba9d9b015c4770497a
Always-Learningeek <myrealemail1400@gmail.com>
2025-02-23 14:27:17 +0100

    accounts app forms developed

---
+++ accounts/forms.py
@@ -0,0 +1,33 @@
+from django.forms import ModelForm
+from captcha.fields import CaptchaField
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+class LoginForm(ModelForm):
+    captcha = CaptchaField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class CustomAuthenticationForm(forms.Form):
+    username_or_email = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    captcha = CaptchaField()
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.models import User
+from accounts.forms import CustomUserCreationForm, CustomAuthenticationForm
 
-# Create your views here.
+
+@csrf_protect
+def login_view(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = CustomAuthenticationForm(request.POST)
+            if form.is_valid():
+                username_or_email = form.cleaned_data.get('username_or_email')
+                password = form.cleaned_data.get('password')
+                try:
+                    if '@' in username_or_email:
+                        user = User.objects.get(email=username_or_email)
+                    else:
+                        user = User.objects.get(username=username_or_email)
+                    user = authenticate(request, username=user.username, password=password)
+                    if user is not None:
+                        login(request, user)
+                        messages.success(request, 'Login successful!')
+                        return redirect('/')
+                    else:
+                        messages.error(request, 'Invalid credentials.')
+                except User.DoesNotExist:
+                    messages.error(request, 'User with this email or username does not exist.')
+            else:
+                messages.error(request, 'Invalid form.')
+        else:
+            form = CustomAuthenticationForm()
+
+        context = {'form': form}
+        return render(request, 'accounts/login.html', context)
+    else:
+        return redirect('/')
 
 
-def index(request):
-    return  render(request, 'website/index.html')
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect('/')
+    else:
+        form = CustomUserCreationForm()
+        context = {'form': form}
+        return render(request, 'accounts/register.html', context)
